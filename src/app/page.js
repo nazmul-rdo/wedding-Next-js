2534309c98ce5d1b3d9f8581f634a478158c2ff0
nazmul-rdo <mdnazmulkhanrdo@gmail.com>
2024-04-05 01:53:04 +0600

    add section

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-    
-    <Link href='/deshboard'> Login Deshboard</Link>
+
+      <Link href='/deshboard'> Login Deshboard</Link>
       <header className="bg-gray-700">
 
         <nav class="flex items-center justify-between flex-wrap p-6 max-w-screen-xl mx-auto">
@@ -58,7 +58,7 @@ export default function Home() {
               </a>
             </div>
             <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-              <img src="https://wpblog.zyro.com/cdn-cgi/img/w=1200,q=85/wp-content/uploads/2020/03/couple-hugging-wedding-fairy-lights.jpg" alt="mockup" />
+              <img src="https://img.freepik.com/free-photo/newlyweds-smiling_1153-1049.jpg?w=900" alt="mockup" />
             </div>
           </div>
         </section>
@@ -71,23 +71,85 @@ export default function Home() {
               <p class="mb-8 text-lg font-normal lg:text-xl sm:px-16 xl:px-48 text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
             </div>
 
-              <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-                <div>
-                  <img className="rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/img.jpg" alt="no img" />
-                </div>
-                <div>
-                  <img className="rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/img.jpg" alt="no img" />
-                </div><div>
-                  <img className="rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/img.jpg" alt="no img" />
-                </div><div>
-                  <img className="rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/img.jpg" alt="no img" />
-                </div><div>
-                  <img className="rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/img.jpg" alt="no img" />
-                </div><div>
-                  <img className="rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/img.jpg" alt="no img" />
-                </div>
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+              <div>
+                <img className="rounded-lg" src="https://img.freepik.com/free-photo/charming-bride-groom-hold-each-other-tender-sitting-bench-outside_8353-7106.jpg?w=1380" alt="no img" />
+              </div>
+              <div>
+                <img className="rounded-lg" src="https://img.freepik.com/free-photo/charming-bride-groom-hold-each-other-tender-sitting-bench-outside_8353-7106.jpg?w=1380" alt="no img" />
+              </div><div>
+                <img className="rounded-lg" src="https://img.freepik.com/free-photo/charming-bride-groom-hold-each-other-tender-sitting-bench-outside_8353-7106.jpg?w=1380" alt="no img" />
+              </div><div>
+                <img className="rounded-lg" src="https://img.freepik.com/free-photo/charming-bride-groom-hold-each-other-tender-sitting-bench-outside_8353-7106.jpg?w=1380" alt="no img" />
+              </div><div>
+                <img className="rounded-lg" src="https://img.freepik.com/free-photo/charming-bride-groom-hold-each-other-tender-sitting-bench-outside_8353-7106.jpg?w=1380" alt="no img" />
+              </div><div>
+                <img className="rounded-lg" src="https://img.freepik.com/free-photo/charming-bride-groom-hold-each-other-tender-sitting-bench-outside_8353-7106.jpg?w=1380" alt="no img" />
               </div>
             </div>
+          </div>
+        </section>
+
+
+        <section class="bg-gray-800">
+          
+        <div class="py-4 px-2 mx-auto max-w-screen-xl text-center lg:py-8 lg:px-6">
+              <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">Our Story</h1>
+            </div>
+          <aside aria-label="Related articles" class="py-2 lg:py-12 bg-gray-800">
+            <div class="px-4 mx-auto max-w-screen-xl">
+              <div class="grid gap-12 sm:grid-cols-2 lg:grid-cols-4 justify-center">
+                <article class="max-w-xs">
+                  <a href="#">
+                    <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-1.png" class="mb-5 rounded-lg" alt="Image 1" />
+                  </a>
+                  <h2 class="mb-2 text-xl font-bold leading-tight text-white">
+                    <a href="#">Our first office</a>
+                  </h2>
+                  <p class="mb-4 text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many changes! After months of preparation.</p>
+                  <a href="#" class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+                    Read in 2 minutes
+                  </a>
+                </article>
+                <article class="max-w-xs">
+                  <a href="#">
+                    <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-2.png" class="mb-5 rounded-lg" alt="Image 2" />
+                  </a>
+                  <h2 class="mb-2 text-xl font-bold leading-tight text-white">
+                    <a href="#">Enterprise design tips</a>
+                  </h2>
+                  <p class="mb-4  text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many changes! After months of preparation.</p>
+                  <a href="#" class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+                    Read in 12 minutes
+                  </a>
+                </article>
+                <article class="max-w-xs">
+                  <a href="#">
+                    <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-3.png" class="mb-5 rounded-lg" alt="Image 3" />
+                  </a>
+                  <h2 class="mb-2 text-xl font-bold leading-tight text-white">
+                    <a href="#">We partnered with Google</a>
+                  </h2>
+                  <p class="mb-4  text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many changes! After months of preparation.</p>
+                  <a href="#" class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+                    Read in 8 minutes
+                  </a>
+                </article>
+                <article class="max-w-xs">
+                  <a href="#">
+                    <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-4.png" class="mb-5 rounded-lg" alt="Image 4" />
+                  </a>
+                  <h2 class="mb-2 text-xl font-bold leading-tight text-white">
+                    <a href="#">Our first project with React</a>
+                  </h2>
+                  <p class="mb-4  text-gray-500 dark:text-gray-400">Over the past year, Volosoft has undergone many changes! After months of preparation.</p>
+                  <a href="#" class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+                    Read in 4 minutes
+                  </a>
+                </article>
+              </div>
+            </div>
+          </aside>
         </section>
       </main>
     </>
